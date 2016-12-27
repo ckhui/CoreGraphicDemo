ae7bc5cb00b70e6e07444f1f04788f153d951e9f
chua khang hui <ckhanghui@gmail.com>
2016-12-27 11:47:11 +0800

    done tutorial

--- CoreGraphicDemo/ViewController.swift
+++ CoreGraphicDemo/ViewController.swift
@@ -10,9 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Counter outlets
+    @IBOutlet weak var counterView: CounterView!
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var graphView: GraphView!
+    
+    var isGraphViewShowing = false
+    
+    @IBOutlet weak var averageWaterDrunk: UILabel!
+    @IBOutlet weak var maxLabel: UILabel!
+    
+    
+    @IBAction func btnPushButton(button: RoundButton) {
+        if button.isAddButton {
+            counterView.counter += 1
+        } else {
+            if counterView.counter > 0 {
+                counterView.counter -= 1
+            }
+        }
+        counterLabel.text = String(counterView.counter)
+        
+        // This makes it so that if the user presses the plus button while the graph is showing, the display will swing back to show the counter.
+        if isGraphViewShowing {
+            counterViewTap(gesture: nil)
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        counterLabel.text = String(counterView.counter)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +53,75 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func counterViewTap(gesture:UITapGestureRecognizer?) {
+        if (isGraphViewShowing) {
+            
+            
+            //hide Graph
+            UIView.transition(from: graphView, to: counterView, duration: 1.0, options: [.transitionFlipFromLeft, .showHideTransitionViews] , completion: nil)
+            
+        } else {
+            
+            //show Graph
+             UIView.transition(from: counterView, to: graphView, duration: 1.0, options: [.transitionFlipFromRight , .showHideTransitionViews] , completion: nil)
+            
+            setupGraphDisplay()
+        }
+        isGraphViewShowing = !isGraphViewShowing
+    }
+    
+   
+    
+    
+    func setupGraphDisplay() {
+        
+        //Use 7 days for graph - can use any number,
+        //but labels and sample data are set up for 7 days
+        let noOfDays:Int = 7
+        
+        //1 - replace last day with today's actual data
+        graphView.graphPoints[graphView.graphPoints.count-1] = counterView.counter
+        
+        //2 - indicate that the graph needs to be redrawn
+        graphView.setNeedsDisplay()
+        
+        maxLabel.text = "\(graphView.graphPoints.max())"
+        
+        //3 - calculate average from graphPoints
+        let average = graphView.graphPoints.reduce(0, +)
+            / graphView.graphPoints.count
+        averageWaterDrunk.text = "\(average)"
+        
+        //set up labels
+        //day of week labels are set up in storyboard with tags
+        //today is last day of the array need to go backwards
+        
+        //4 - get today's day number
+//        let dateFormatter = NSDateFormatter()
+//        let calendar = NSCalendar.currentCalendar()
+//        let componentOptions:NSCalendarUnit = .CalendarUnitWeekday
+//        let components = calendar.components(componentOptions,
+//                                             fromDate: NSDate())
+//        var weekday = components.weekday
+        var weekday = 6
+        
+        let days = ["S", "S", "M", "T", "W", "T", "F"]
+        
+        //5 - set up the day name labels with correct day
+        for i in 1...days.count {
+            if let labelView = graphView.viewWithTag(i) as? UILabel {
+                if weekday == 7 {
+                    weekday = 0
+                }
+                
+                labelView.text = days[weekday]
+                weekday -= 1
+                if weekday < 0 {
+                    weekday = days.count - 1
+                }
+            }
+        }
+    }
 
 }
 
